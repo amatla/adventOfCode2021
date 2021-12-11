0e5a2fc1a548bfa03408dfdb596a2c1649b12261
amatla <amatlaow@gmail.com>
2021-12-11 17:26:49 +0100

    Day3 solution 1 refactored and added comments

--- 3/3.js
+++ 3/3.js
@@ -12,43 +12,31 @@ const test = [
   '10000',
   '11001',
   '00010',
-  '01010'];
+  '01010',
+];
 
-  const test2 = ['00100', '11110'] // [1,1] [1,1] [0, 2] [1,1] [2, 0]
-
+// retrive input as an array of strings
 const values = fs
   .readFileSync('./input.txt')
   .toString('utf-8')
-  .split('\n').map(str => str.toString(2));
+  .split('\n');
 
-  function powerConsuption(input){
-    const len = input.length;
-    const bit_len = input[0].length;
-    const bits = [];
-    for(let i = 0; i < bit_len; i += 1){
-      bits.push([0,0]);
+// solution 1
+function powerConsuption(input) {
+  let gamma = '';
+  let epsilon = '';
+
+  for (let i = 0, len = input[0].length; i < len; i += 1) {
+    const bits = [0, 0];
+    for (let j = 0, arrLen = input.length; j < arrLen; j += 1) {
+      if (input[j][i] === '0') bits[0] += 1;
+      else bits[1] += 1;
     }
-    
-    for(let i = 0; i < len; i += 1){
-      for(let j = 0; j < bit_len; j += 1){
-        if(input[i][j] === '0') {
-          bits[j][0] += 1;
-        } else bits[j][1] += 1;
-      }
-    }
-    let gamma = '';
-    let epsilon = '';
-    for(let i = 0; i < bit_len; i += 1){
-      if(bits[i][0] > bits[i][1]) {
-        gamma = gamma.concat('0');
-        epsilon = epsilon.concat('1');
-      }
-      else {
-        gamma = gamma.concat('1');
-        epsilon = epsilon.concat('0');
-      }
-    }
-    return parseInt(gamma, 2) * parseInt(epsilon, 2);
+    gamma += bits[0] > bits[1] ? '0' : '1';
+    epsilon += bits[0] > bits[1] ? '1' : '0';
   }
+  return parseInt(gamma, 2) * parseInt(epsilon, 2);
+}
 
- console.log(powerConsuption(values));
+// solution 1
+console.log(powerConsuption(values));
