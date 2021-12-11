eb237f78d87307475269c88dbd283868eb34d7a6
amatla <amatlaow@gmail.com>
2021-12-11 16:18:45 +0100

    Day 1 refactored and added comments

--- 1/1.js
+++ 1/1.js
@@ -2,30 +2,30 @@ const fs = require('fs');
 
 // const test = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
+// retrive input data as an array of integers
 const values = fs
   .readFileSync('./input.txt')
   .toString('utf-8')
   .split('\n')
   .map((str) => parseInt(str, 10));
 
+// solution for part 1
 function depth(input) {
-  let count = 0;
-  for (let i = 0, n = input.length; i < n - 1; i += 1) {
-    if (input[i + 1] > input[i]) count += 1;
-  }
-  return count;
+  return input.reduce((prev, curr, index) => {
+    if (input[index] > input[index - 1] && index > 0) return prev + 1;
+    return prev;
+  }, 0);
 }
 
+// solution for part 2
+// create an array of the samples to use as input for the depth function
 function sample(input) {
-  const result = [];
-  for (let i = 0, n = input.length; i < n - 2; i += 1) {
-    let tmp = input[i];
-    for (let j = 1; j < 3; j += 1) {
-      tmp += input[i + j];
-    }
-    result.push(tmp);
-  }
-  return result;
+  return input.map((curr, index, arr) => {
+    if (index > arr.length - 3) return curr;
+    let tmp = 0;
+    for (let i = 1; i < 3; i += 1) tmp += arr[index + i];
+    return curr + tmp;
+  });
 }
 
 // solution for part 1
