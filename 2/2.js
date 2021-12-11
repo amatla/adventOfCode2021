aef1d47b55e4101488d9cbb4a2aac6187038d31e
amatla <amatlaow@gmail.com>
2021-12-11 16:32:27 +0100

    Day 2 refactored and added comments

--- 2/2.js
+++ 2/2.js
@@ -1,51 +1,73 @@
 const fs = require('fs');
 
-const test = [['forward', '5'], ['down', '5'], ['forward', '8'], ['up', '3'], ['down', '8'], ['forward', '2']];
-
+// retrive input values as an array where each element is
+// an array containing 2 string values: the direction and the value of the movement.
 const values = fs
   .readFileSync('./input.txt')
   .toString('utf-8')
   .split('\n')
   .map((str) => str.split(' '));
 
-function move(moveList){
+// const test = [
+//   ['forward', '5'],
+//   ['down', '5'],
+//   ['forward', '8'],
+//   ['up', '3'],
+//   ['down', '8'],
+//   ['forward', '2'],
+// ];
+
+// solution 1
+function move(moveList) {
   let horizontal = 0;
   let depth = 0;
 
-  for(let i = 0, n = moveList.length; i < n; i += 1){
-    const val = parseInt(moveList[i][1], 10)
-    switch(moveList[i][0]){
-      case 'forward': horizontal += val;
-      break;
+  for (let i = 0, n = moveList.length; i < n; i += 1) {
+    const val = parseInt(moveList[i][1], 10);
+    switch (moveList[i][0]) {
+      case 'forward':
+        horizontal += val;
+        break;
 
-      case 'up': depth -= val;
-      break;
+      case 'up':
+        depth -= val;
+        break;
 
-      case 'down': depth += val;
-      break;
+      case 'down':
+        depth += val;
+        break;
+
+      default:
+        break;
     }
   }
   return horizontal * depth;
 }
 
-function moveAim(moveList){
+// solution 2
+function moveAim(moveList) {
   let horizontal = 0;
   let depth = 0;
   let aim = 0;
 
-  for(let i = 0, n = moveList.length; i < n; i += 1){
-    const val = parseInt(moveList[i][1], 10)
-    switch(moveList[i][0]){
-      case 'forward': 
-      horizontal += val;
-      depth += aim * val
-      break;
+  for (let i = 0, n = moveList.length; i < n; i += 1) {
+    const val = parseInt(moveList[i][1], 10);
+    switch (moveList[i][0]) {
+      case 'forward':
+        horizontal += val;
+        depth += aim * val;
+        break;
 
-      case 'up': aim -= val;
-      break;
+      case 'up':
+        aim -= val;
+        break;
 
-      case 'down': aim += val;
-      break;
+      case 'down':
+        aim += val;
+        break;
+
+      default:
+        break;
     }
   }
   return horizontal * depth;
