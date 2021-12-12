820fddf0a2decd4a5e06400a7b0a76d3dcdecc0b
amatla <amatlaow@gmail.com>
2021-12-12 16:43:46 +0100

    Day 3 solution

--- 3/3.js
+++ 3/3.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
 
-const test = [
-  '00100',
-  '11110',
-  '10110',
-  '10111',
-  '10101',
-  '01111',
-  '00111',
-  '11100',
-  '10000',
-  '11001',
-  '00010',
-  '01010',
-];
+// const test = [
+//   '00100',
+//   '11110',
+//   '10110',
+//   '10111',
+//   '10101',
+//   '01111',
+//   '00111',
+//   '11100',
+//   '10000',
+//   '11001',
+//   '00010',
+//   '01010',
+// ];
 
 // retrive input as an array of strings
 const values = fs
@@ -38,5 +38,38 @@ function powerConsuption(input) {
   return parseInt(gamma, 2) * parseInt(epsilon, 2);
 }
 
+// filters an array based on what the most significant bit at position 'pos' is
+// if the rating is 'oxygen'
+// filters the array based on what the least significant bit at positon 'pos' is
+// if the rating is 'co2'
+function filter(arr, pos, rating) {
+  const one = [];
+  const zero = [];
+  for (let i = 0, len = arr.length; i < len; i += 1) {
+    if (arr[i][pos] === '1') one.push(arr[i]);
+    else zero.push(arr[i]);
+  }
+  if (rating === 'oxygen')
+    return one.length >= zero.length ? one : zero;
+  if (rating === 'co2') return one.length >= zero.length ? zero : one;
+  return console.error("rating must be 'oxygen' or 'co2'");
+}
+
+// solution 2
+function lifeSupport(input) {
+  let pos = 0;
+  let oxygen = filter(input, pos, 'oxygen');
+  let co2 = filter(input, pos, 'co2');
+  while (oxygen.length > 1 || co2.length > 1) {
+    pos += 1;
+    if (oxygen.length > 1) oxygen = filter(oxygen, pos, 'oxygen');
+    if (co2.length > 1) co2 = filter(co2, pos, 'co2');
+  }
+  return parseInt(oxygen, 2) * parseInt(co2, 2);
+}
+
 // solution 1
 console.log(powerConsuption(values));
+
+// solution 2
+console.log(lifeSupport(values));
